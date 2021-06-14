d7d381e74bf78fcc707581d1c0deb394482bed76
Justin Li <justinli@Justins-MBP.attlocal.net>
2021-06-13 21:16:38 -0500

    Added logic for processing entries

--- src/pages/rank-page-proto/ranking.ts
+++ src/pages/rank-page-proto/ranking.ts
@@ -15,13 +15,41 @@ export class RankList implements OnInit {
     ) {}
     ngOnInit(){
         this.http.get(this._apiURL).subscribe((data: RankingAPI) => {
-            this.theTodo = data ;
-            
+            this.theTodo = data;
+            var weekbonus = 0;
             this.theTodo.data.forEach(item => {
                 if (item.points == undefined) {
                     item.points = 0;
                 }
-            })
+                else {
+                    item.detail.forEach(entry => {
+                        if (entry.points >3600 ){
+                            entry.points = 3600;
+                        }
+                    })
+                    let weekcount = new Date (item.detail[0].workoutDate);
+                    var boncount=0;
+                    var pointtally = 0;
+                    var weektally = 0;
+                    item.detail.forEach(entry2 => {
+                        if (entry2.points > 600){
+                            boncount++;
+                        }
+                        pointtally+=entry2.points;
+                        weektally+=entry2.points*.1;
+                        let dayCount= new Date (entry2.workoutDate);
+                        if ((weekcount.getTime()-dayCount.getTime())<=0){
+                            weekcount = dayCount;
+                            if (boncount >= 3) {
+                                boncount=0;
+                                pointtally+=weektally;
+                            }
+                            weektally = 0;
+                        }
+                    })
+                    item.points=pointtally;
+                }
+             })
             
             console.log(this.theTodo.data);
         }) ;
